4e5d50527b8d491f0e11ff18e6a95c3075a0fa2c
Riley Barabash <admin@rileybarabash.com>
2025-08-22 12:30:28 -0600

    build: bump production

--- apps/launcher/src/config/environment.ts
+++ apps/launcher/src/config/environment.ts
@@ -7,7 +7,7 @@ import type { EnvironmentConfig } from "../types/config"
 export const environment = {
     mode: "development",
     baseUrl: {
-        development: "https://get-from-devtunnels-cli.devtunnels.ms",
-        production: "https://osynco.app"
+        development: "https://7wbhc8w7-6462.usw2.devtunnels.ms",
+        production: "https://osynco.com"
     }
 } satisfies EnvironmentConfig
